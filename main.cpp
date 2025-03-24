b49b2106a186ee6d9203b8578b1a6f52347cd83e
Federico Barbetti <federico.barbetti@correounivalle.edu.co>
2025-03-24 12:11:42 -0500

    se desarrolla la funcion avalaible_address que seleccionara aleatoriamente las 5 posiciones de memoria a ser usadas (tomadas de un conjunto de 10)

--- main.cpp
+++ main.cpp
@@ -8,9 +8,50 @@
 #include "Vlr_ins.h"
 
 using namespace std;
+/**
+ * @brief inicia gestor random
+ * 
+ */
+void inicializar_ran(){
+    srand(time(NULL));
+}
+
+/**
+ * @brief total de posiciones de memoria
+ * que se usaran del total de disponibles
+ *  */
+const int cant_dir = 5;
+
+string avalaible_address[](){
+
+    /**
+     * @brief arreglo con las posiciones de memoria
+     * disponibles
+     */
+    std::string aval_addr[10] = {"D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10"};
+    /**
+     * @brief Posiciones de memoria
+     * seleccionadas para usar
+     */
+    std::string addr_mem[cant_dir]{};
+
+
+
+
+
+
+    return fin_mem[5];
+}
+
+
+
+
 
 
 int main(){
+
+    inicializar_ran();
+    
 
     Prs_run *pr;
     pr = new Prs_run();
@@ -22,7 +63,6 @@ int main(){
     
     int limite = 100;
 
-    srand(time(0));
     
     for (int i = 0; i < 6; i++)
     {
